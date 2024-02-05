7b00a8acea67a1ad4b1b813231fa6e41ccbf2266
Umid Olimzhanov <uolimzhanov@gmail.com>
2024-02-05 23:24:51 +0500

    proj: fix naming

--- app/src/main/java/com/uolimzhanov/eshopeffectivemobile/ui/theme/Color.kt
+++ app/src/main/java/com/uolimzhanov/eshopeffectivemobile/ui/theme/Color.kt
@@ -3,9 +3,9 @@ package com.uolimzhanov.eshopeffectivemobile.ui.theme
 import androidx.compose.ui.graphics.Color
 
 
-val Grey = Color(160, 161, 163)
-val LightGrey = Color(248, 248, 248)
-val DarkGrey = Color(62, 62, 62)
+val Gray = Color(160, 161, 163)
+val LightGray = Color(248, 248, 248)
+val DarkGray = Color(62, 62, 62)
 val Pink = Color(214, 47, 137)
 val LightPink = Color(255, 138, 201)
 val DarkBlue = Color(82, 96, 109)
--- app/src/main/java/com/uolimzhanov/eshopeffectivemobile/ui/theme/Theme.kt
+++ app/src/main/java/com/uolimzhanov/eshopeffectivemobile/ui/theme/Theme.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.graphics.Color
 private val EMColorScheme = lightColorScheme(
     primary = Pink,
     secondary = LightPink,
-    tertiary = LightGrey,
+    tertiary = LightGray,
     onPrimary = Color.White,
     surface = Color.White,
 
