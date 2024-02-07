393d5cfb5ea8a3bb9b62b80c23dc677396e9127c
Umid Olimzhanov <uolimzhanov@gmail.com>
2024-02-07 22:56:03 +0500

    login: visual transformation for phone field

---
+++ app/src/main/java/com/uolimzhanov/eshopeffectivemobile/ui/utils/PhoneNumberVisualTransformation.kt
@@ -0,0 +1,63 @@
+package com.uolimzhanov.eshopeffectivemobile.ui.utils
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+
+/**
+ * created by uolimzhanov on 05.02.2024
+ */
+class PhoneNumberVisualTransformation(val mask: String, val maskCharacter: Char) : VisualTransformation {
+
+    private val maxLength = mask.count { it == maskCharacter }
+
+    override fun filter(text: AnnotatedString): TransformedText {
+        val trimmed = if (text.length > maxLength) text.take(maxLength) else text
+
+        val annotatedString = buildAnnotatedString {
+            if (trimmed.isEmpty()) return@buildAnnotatedString
+
+            var maskIndex = 0
+            var textIndex = 0
+            while (textIndex < trimmed.length && maskIndex < mask.length) {
+                if (mask[maskIndex] != maskCharacter) {
+                    val nextDigitIndex = mask.indexOf(maskCharacter, maskIndex)
+                    append(mask.substring(maskIndex, nextDigitIndex))
+                    maskIndex = nextDigitIndex
+                }
+                append(trimmed[textIndex++])
+                maskIndex++
+            }
+        }
+
+        return TransformedText(annotatedString, PhoneOffsetMapper(mask, maskCharacter))
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is PhoneNumberVisualTransformation) return false
+        if (mask != other.mask) return false
+        return maskCharacter == other.maskCharacter
+    }
+
+    override fun hashCode(): Int {
+        return mask.hashCode()
+    }
+}
+
+private class PhoneOffsetMapper(val mask: String, val numberChar: Char) : OffsetMapping {
+
+    override fun originalToTransformed(offset: Int): Int {
+        var noneDigitCount = 0
+        var i = 0
+        while (i < offset + noneDigitCount) {
+            if (mask[i++] != numberChar) noneDigitCount++
+        }
+        return offset + noneDigitCount
+    }
+
+    override fun transformedToOriginal(offset: Int): Int =
+        offset - mask.take(offset).count { it != numberChar }
+}
